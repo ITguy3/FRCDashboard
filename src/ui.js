a35bb2dd2dbca370a2cf07bf4f08d3af03ebe24f
ITguy3 <ithangalvadi@outlook.com>
2025-11-18 18:15:55 -0500

    Update ui.js

--- src/ui.js
+++ src/ui.js
@@ -21,7 +21,10 @@ NetworkTables.addKeyListener('/SmartDashboard/luniteCount', (key, value) => {
 NetworkTables.addKeyListener('/SmartDashboard/gameTime', (key, value) => {
     // This is an example of how a dashboard could display the remaining time in a match.
     // We assume here that value is an integer representing the number of seconds left.
-    temp = 135 - value
+
+    // 135s -> 2:15, this gameTime value will start from 0 and count up
+    // since teleop lasts 2:15, just subtract gameTime from 135 sec
+    temp = 135 - value 
     ui.timer.textContent = temp < 0 ? '0:00' : Math.floor(temp / 60) + ':' + (temp % 60 < 10 ? '0' : '') + parseInt(temp % 60);
 });
 
